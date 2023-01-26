2404cde959d4a663c191d0e5f7f6d68072571b70
Stepko <sguralmen@gmail.com>
2023-01-26 23:19:25 +0200

    Fixing

--- Core/Src/sLog.c
+++ Core/Src/sLog.c
@@ -1,30 +1,26 @@
 #include "sLog.h"
-
+typedef struct{
+      char textToTransmit[S_LOG_FIFO_SIZE];
+      uint16_t size;
+}FIFO;
 struct{
   uint16_t startPoint;
   uint16_t endPoint;
-  char textToTransmit[S_LOG_FIFO_AMOUNT][S_LOG_FIFO_SIZE];
-  uint16_t size;
-
-    struct{
-      char textToTransmit[S_LOG_FIFO_SIZE];
-      uint16_t size;
-    }FIFO[S_LOG_FIFO_AMOUNT];
-
+  FIFO fifo[S_LOG_FIFO_AMOUNT];
 }sLog;
 
 void sLogPrint(char* text,...){
 	va_list (arg);
 	va_start(arg, text);
-		vsprintf(sLog.FIFO[sLog.endPoint].textToTransmit, text, arg);
+		vsprintf(sLog.fifo[sLog.endPoint].textToTransmit, text, arg);
 		va_end(arg);
-		sLog.FIFO[sLog.endPoint].size = strlen(sLog.FIFO[sLog.endPoint].textToTransmit);
+		sLog.fifo[sLog.endPoint].size = strlen(sLog.fifo[sLog.endPoint].textToTransmit);
 	sLog.endPoint++;
 }
 
 void sLogUpdate(){
 	if(sLog.startPoint != sLog.endPoint)
-	for(uint16_t point = sLog.startPoint; point != sLog.endPoint+1; point = (point+1)%S_LOG_FIFO_AMOUNT){
-	CDC_Transmit_FS(sLog.FIFO[point].textToTransmit,sLog.FIFO[point].size);}
+	for(uint16_t point = sLog.startPoint; point != (sLog.endPoint+1)%S_LOG_FIFO_AMOUNT; point = (point+1)%S_LOG_FIFO_AMOUNT){
+	CDC_Transmit_FS(sLog.fifo[point].textToTransmit,sLog.fifo[point].size);}
 	sLog.startPoint = sLog.endPoint;
 }
