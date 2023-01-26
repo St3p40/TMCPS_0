4b72b2741b2047c8a0a93e641554c6d7c87d336c
Stepko <sguralmen@gmail.com>
2023-01-26 12:51:13 +0200

    Putting sLog Variables into structure

--- Core/Src/sLog.c
+++ Core/Src/sLog.c
@@ -1,20 +1,22 @@
 #include "sLog.h"
+struct{
 uint16_t Point;
 char textTotransmit[128][256];
 uint8_t textlen[128];
+}sLog;
 void sLogPrint(char* text,...){
 	va_list (arg);
 	va_start(arg, text);
-		vsprintf(&textTotransmit[Point][0], text, arg);
+		vsprintf(&sLog.textTotransmit[sLog.Point][0], text, arg);
 		va_end(arg);
-		textlen[Point] = strlen(&textTotransmit[Point][0]);
-		Point++;
+		sLog.textlen[sLog.Point] = strlen(&sLog.textTotransmit[sLog.Point][0]);
+		sLog.Point++;
 }
 
 void sLogUpdate(){
-	if(Point)
-	for(uint8_t i = 0; i < Point + 1; i++){
-	CDC_Transmit_FS(&textTotransmit[i][0],textlen[i]);
+	if(sLog.Point)
+	for(uint8_t i = 0; i < sLog.Point + 1; i++){
+	CDC_Transmit_FS(&sLog.textTotransmit[i][0],sLog.textlen[i]);
 	}
-	Point = 0;
+	sLog.Point = 0;
 }
