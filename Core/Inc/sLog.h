91eddd660710170766d2aa49ab60d4befbc600a5
Stepko <sguralmen@gmail.com>
2023-01-27 07:22:24 +0200

    renaming variables and defines

--- Core/Inc/sLog.h
+++ Core/Inc/sLog.h
@@ -4,7 +4,7 @@
 #include "stdarg.h"
 #include "stdio.h"
 
-#define S_LOG_FIFO_BUF_SIZE 128
+#define S_LOG_FIFO_MSG_BUF_SIZE 128
 #define S_LOG_FIFO_OBJ_AMOUNT 256
 
 void sLogPrint(char* text,...);
--- Core/Src/sLog.c
+++ Core/Src/sLog.c
@@ -1,8 +1,8 @@
 #include "sLog.h"
 
 typedef struct{
-  char textToTransmit[S_LOG_FIFO_BUF_SIZE];
-  uint16_t size;
+  char msgBuff[S_LOG_FIFO_MSG_BUF_SIZE];
+  uint16_t msgSize;
 }FIFO;
 
 struct{
@@ -14,16 +14,16 @@ struct{
 void sLogPrint(char* text,...){
   va_list (arg);
   va_start(arg, text);
-  vsprintf(sLog.fifo[sLog.endPoint].textToTransmit, text, arg);
+  vsprintf(sLog.fifo[sLog.endPoint].msgBuff, text, arg);
   va_end(arg);
-  sLog.fifo[sLog.endPoint].size = strlen(sLog.fifo[sLog.endPoint].textToTransmit);
+  sLog.fifo[sLog.endPoint].msgSize = strlen(sLog.fifo[sLog.endPoint].msgBuff);
   sLog.endPoint=sLog.endPoint%S_LOG_FIFO_OBJ_AMOUNT;
 }
 
 void sLogUpdate(){
   if(sLog.startPoint != sLog.endPoint)
   for(uint16_t point = sLog.startPoint; point != sLog.endPoint; point = (point+1)%S_LOG_FIFO_OBJ_AMOUNT){
-    CDC_Transmit_FS(sLog.fifo[point].textToTransmit,sLog.fifo[point].size);
+    CDC_Transmit_FS(sLog.fifo[point].msgBuff,sLog.fifo[point].msgSize);
   }
   sLog.startPoint = sLog.endPoint;
 }
