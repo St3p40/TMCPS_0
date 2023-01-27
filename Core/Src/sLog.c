ad2c05807ff6caa293cc24bde330a5ba868616e7
Stepko <sguralmen@gmail.com>
2023-01-28 00:06:09 +0200

    Update sLog.c

--- Core/Src/sLog.c
+++ Core/Src/sLog.c
@@ -20,12 +20,11 @@ void sLogPrint(char* text,...){
     sLog.fifo[sLog.endPoint].msgBuff[S_LOG_FIFO_MSG_BUF_SIZE - 2] = '\n';
   	sLog.fifo[sLog.endPoint].msgSize = S_LOG_FIFO_MSG_BUF_SIZE;
   }
-  else{
-	sLog.fifo[sLog.endPoint].msgSize = messageSize;
+  else if(messageSize < 1){
+    sLog.fifo[sLog.endPoint].msgSize = sprintf(sLog.fifo[sLog.endPoint].msgBuff,"sLogErr:%d",messageSize);
   }
-  if(messageSize < 1){
-	sLog.fifo[sLog.endPoint].msgSize = sprintf(sLog.fifo[sLog.endPoint].msgBuff,"sLogErr%d",messageSize);
-  }
+  else
+    sLog.fifo[sLog.endPoint].msgSize = messageSize;
   sLog.endPoint = (sLog.endPoint + 1) % S_LOG_FIFO_OBJ_AMOUNT;
 }
 
